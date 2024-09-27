683e1fd5d8b083fdd0c86ed72abf931c7b12f3f0
Phạm Thanh Trúc <pthanhtruca3@gmail.com>
2024-09-28 03:34:39 +0700

    Update LocationDTO.java

--- src/main/java/disasterwarning/com/vn/models/dtos/LocationDTO.java
+++ src/main/java/disasterwarning/com/vn/models/dtos/LocationDTO.java
@@ -1,4 +1,23 @@
 package disasterwarning.com.vn.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import disasterwarning.com.vn.models.entities.DisasterWarning;
+import disasterwarning.com.vn.models.entities.User;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class LocationDTO {
+
+    private int locationId;
+    private String locationName;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+    @JsonBackReference
+    private UserDTO user;
+
+    @JsonBackReference
+    private List<DisasterWarningDTO> disasterWarnings;
 }
