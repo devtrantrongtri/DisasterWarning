3b4e557d3dc2ac4768a7ba24353ff25be893b1b4
Phạm Thanh Trúc <pthanhtruca3@gmail.com>
2024-09-28 03:33:14 +0700

    Update DisasterDTO.java

--- src/main/java/disasterwarning/com/vn/models/dtos/DisasterDTO.java
+++ src/main/java/disasterwarning/com/vn/models/dtos/DisasterDTO.java
@@ -1,4 +1,19 @@
 package disasterwarning.com.vn.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import java.util.List;
+
 public class DisasterDTO {
+
+    private int disasterId;
+    private String disasterName;
+    private String imageUrl;
+    private String description;
+
+    @JsonBackReference
+    private List<DisasterInfoDTO> disasterInfos;
+
+    @JsonBackReference
+    private List<DisasterWarningDTO> disasterWarnings;
 }
