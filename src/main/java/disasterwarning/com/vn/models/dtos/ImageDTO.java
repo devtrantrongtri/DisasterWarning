6b8746967c92647f5b92a0d16aa7a3d757823e61
Phạm Thanh Trúc <pthanhtruca3@gmail.com>
2024-09-28 03:34:22 +0700

    Update ImageDTO.java

--- src/main/java/disasterwarning/com/vn/models/dtos/ImageDTO.java
+++ src/main/java/disasterwarning/com/vn/models/dtos/ImageDTO.java
@@ -1,4 +1,14 @@
 package disasterwarning.com.vn.models.dtos;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import disasterwarning.com.vn.models.entities.DisasterInfo;
+import jakarta.persistence.*;
+
 public class ImageDTO {
+
+    private int imageId;
+    private String imageUrl;
+
+    @JsonBackReference
+    private DisasterInfoDTO disasterInfo;
 }
