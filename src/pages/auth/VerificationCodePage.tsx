f5f87173ecabc9abd741ee840077fb5fbbce1c1e
devtrantrongtri <devtrantrongtri@gmail.com>
2024-11-04 18:54:12 +0700

    update verify code ui

--- src/pages/auth/VerificationCodePage.tsx
+++ src/pages/auth/VerificationCodePage.tsx
@@ -1,18 +1,64 @@
-import React from 'react';
-import { Button, Typography, Box, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Button, Box } from '@mui/material';
 import AuthLayout from '../../layouts/AuthLayout';
-
+import { MuiOtpInput } from 'mui-one-time-password-input';
 
 const VerificationCodePage: React.FC = () => {
+  const [otp, setOtp] = useState('');
+  const [subtitle,setSubtitle] = useState('We have just sent a verification code to jonathandoe@gmail.com')
+  const handleChange = (newValue: string) => {
+    setOtp(newValue);
+  };
+
+  const handleVerifyCode = () => {
+    setSubtitle("da xac thuc thanh cong :" + otp)
+    // alert("Da gui otp : " + otp);
+  };
+
+  const handleResend = () => {
+    setOtp('');
+    // Add resend code logic o day
+  };
+
   return (
-    <AuthLayout title="Enter Verification Code" subtitle="We have just sent a verification code to jonathandoe@gmail.com">
-      <Box sx={{ display: 'flex', justifyContent: 'center', gap: 1 }}>
-        {[...Array(4)].map((_, index) => (
-          <TextField key={index} variant="outlined" inputProps={{ maxLength: 1 }} />
-        ))}
+    <AuthLayout
+      title="Enter Verification Code"
+      subtitle={subtitle}
+    >
+      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+      <MuiOtpInput
+          value={otp}
+          onChange={handleChange}
+          length={4}
+          validateChar={(value) => /\d/.test(value)} // Chỉ chấp nhận số
+          onComplete={handleVerifyCode}
+          autoFocus
+          TextFieldsProps={{
+            variant: 'outlined',
+            sx: {
+              width: '64px', // Tăng chiều rộng
+              height: '64px', // Tăng chiều cao
+              marginRight: '8px',
+              '& .MuiInputBase-input': {
+                textAlign: 'center',
+                fontSize: '24px', // Tăng kích thước chữ
+              },
+              '& .MuiOutlinedInput-root': {
+                '& fieldset': {
+                  // Thêm bóng bên trong
+                  boxShadow: 'inset 0 0 5px rgba(0, 0, 0, 0.2)',
+                },
+              },
+            },
+          }}
+        />
       </Box>
-      <Button variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>Verify</Button>
-      <Button variant="text" sx={{ mt: 2 }}>Resend Code</Button>
+      {/* <Button variant="contained" color="primary" fullWidth sx={{ mt: 2 }} onClick={handleVerifyCode}> 
+        Verify
+      </Button> */}
+      <Button variant="text" onClick={handleResend} fullWidth sx={{ mt: 2 }}>
+        Resend Code
+      </Button>
     </AuthLayout>
   );
 };
